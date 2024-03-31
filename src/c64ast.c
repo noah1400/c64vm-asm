5e175ec2ae1115c11d8999a21654d039dd7c5c8d
noah1400 <noah.scholz.ns@gmail.com>
2024-03-31 20:07:00 +0200

    start ast

---
+++ src/c64ast.c
@@ -0,0 +1,3 @@
+#include <c64asm_ast.h>
+
+ASTNode *ast_head = NULL;
--- src/include/c64asm_ast.h
+++ src/include/c64asm_ast.h
@@ -1,12 +1,46 @@
 #ifndef c64asm_ast_h
 #define c64asm_ast_h
 
+#include <stdint.h>
+#include <stdlib.h>
+
 typedef enum {
-    INST_LDI,
-    INST_ST,
-    INST_PUSH,
-    INST_POP,
-    INST_JMP
-} InstructionType;
+    NODE_LABEL_DEF,
+    NODE_LABEL_REF,
+    NODE_INSTRUCTION,
+    NODE_IMMEDIATE,
+    NODE_REGISTER,
+    NODE_COMMENT, // added for optional use later
+} NodeType;
+
+typedef struct ASTNode {
+    NodeType type;
+    union {
+        char *label;
+        struct {
+            char *mnemonic;
+            uint16_t opcode;
+            struct ASTNode *operands[2];
+        } instruction;
+        uint64_t immediate;
+        uint8_t reg;
+        char *comment; // added for optional use later
+    } data;
+    struct ASTNode *next;
+} ASTNode;
+
+extern ASTNode *ast_head;
+
+ASTNode *ast_create_node(NodeType type);
+ASTNode *ast_create_label_def_node(char *label);
+ASTNode *ast_create_label_ref_node(char *label);
+ASTNode *ast_create_instruction_node(char *mnemonic, uint16_t opcode, ASTNode *op1, ASTNode *op2);
+ASTNode *ast_create_immediate_node(uint64_t immediate);
+ASTNode *ast_create_register_node(uint8_t reg);
+ASTNode *ast_create_comment_node(char *comment); // added for optional use later
+void ast_append_node(ASTNode **head, ASTNode *node);
+void ast_free(ASTNode *head);
+
+
 
 #endif // c64asm_ast_h
