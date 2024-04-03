9e44ebd1507fd17cc7fcba3bf4c25089f2701d3a
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2024-04-03 09:43:43 +0200

    Update c64gen.c

--- src/c64gen.c
+++ src/c64gen.c
@@ -68,8 +68,60 @@ void c64gen_write_instruction(ASTNode *instruction, FILE *f, uint64_t *ip)
 
 void c64gen_write_register(ASTNode *reg,FILE *f, uint64_t *ip)
 {
-    uint8_t reg_num = reg->data.reg+2;
+    uint8_t reg_num = reg->data.reg+1;
     c64gen_write8(f, reg_num, ip);
+}
+
+void c64gen_first_pass(ASTNode *node, uint64_t *ip)
+{
+    if (!node)
+    {
+        return;
+    }
+    printf("FIRST PASS: NODE AT IP: %lu\n", *ip);
+    switch (node->type)
+    {
+    case NODE_LABEL_DEF:
+        printf("FIRST PASS: Label Definition: %s\n", node->data.label);
+        Symbol *label = symbol_table_find(symbol_table, node->data.label);
+        if (!label)
+        {
+            fprintf(stderr, "Undefined label %s\n", node->data.label);
+            exit(1);
+        }
+        printf("Set address for label %s: %lu\n", node->data.label, *ip);
+        label->address = *ip;
+        break;
+    case NODE_LABEL_REF:
+        printf("FIRST PASS: Label Reference: %s\n", node->data.label);
+        Symbol *ref = symbol_table_find(symbol_table, node->data.label);
+        if (!ref)
+        {
+            fprintf(stderr, "Undefined label %s\n", node->data.label);
+            exit(1);
+        }
+        *ip += sizeof(uint64_t);
+        break;
+    case NODE_INSTRUCTION:
+        printf("FIRST PASS: Instruction: %s\n", node->data.instruction.mnemonic);
+        *ip += sizeof(uint16_t);
+        ASTNode *op1 = node->data.instruction.operands[0];
+        ASTNode *op2 = node->data.instruction.operands[1];
+        c64gen_first_pass(op1, ip);
+        c64gen_first_pass(op2, ip);
+        break;
+    case NODE_IMMEDIATE:
+        *ip += sizeof(uint64_t);
+        break;
+    case NODE_REGISTER:
+        *ip += sizeof(uint8_t);
+        break;
+    default:
+        fprintf(stderr, "Unknown AST node type %d\n", node->type);
+        exit(1);
+    }
+
+    
 }
 
 void c64gen_write_node(ASTNode *node, FILE *f, uint64_t *ip)
@@ -79,22 +131,23 @@ void c64gen_write_node(ASTNode *node, FILE *f, uint64_t *ip)
     case NODE_LABEL_DEF:
         printf("GEN: Label Definition: %s\n", node->data.label);
         Symbol *label = symbol_table_find(symbol_table, node->data.label);
-        if (!label)
+        if (label->address != *ip)
         {
-            fprintf(stderr, "Undefined label %s\n", node->data.label);
+            fprintf(stderr, "Address mismatch for label %s: %lu != %lu\n", node->data.label, label->address, *ip);
             exit(1);
         }
-        label->address = *ip;
+        // Do nothing (labels are handled in the first pass)
         break;
     case NODE_LABEL_REF:
         printf("GEN: Label Reference: %s\n", node->data.label);
         Symbol *ref = symbol_table_find(symbol_table, node->data.label);
-            if (!ref)
-            {
-                fprintf(stderr, "Undefined label %s\n", node->data.label);
-                exit(1);
-            }
-            c64gen_write64(f, ref->address, ip);
+        if (!ref)
+        {
+            fprintf(stderr, "Undefined label %s\n", node->data.label);
+            exit(1);
+        }
+        printf("Writing address for label %s: %lu\n", node->data.label, ref->address);
+        c64gen_write64(f, ref->address, ip);
         break;
     case NODE_INSTRUCTION:
         printf("GEN: Instruction: %s\n", node->data.instruction.mnemonic);
@@ -129,8 +182,24 @@ void c64gen_gen(ASTNode *ast, const char *filename)
     // Initialize the instruction pointer to 0
     uint64_t ip = 0;
 
+
+
     // Traverse the AST and generate code
     ASTNode *current = ast;
+
+    while (current != NULL)
+    {
+        // Scan code and set label addresses
+        c64gen_first_pass(current, &ip);
+        // Move to the next node in the AST
+        current = current->next;
+    }
+
+    // Reset the instruction pointer to 0
+    ip = 0;
+    // Reset the current node to the start of the AST
+    current = ast;
+
     while (current != NULL)
     {
         // Write the current node to the output file
